acacbf6c00e00cdb8b4eb0de5bd1d53f0adf9413
John Harold Remigio <168771057+NoctruneJohnH05@users.noreply.github.com>
2025-12-10 20:04:22 -0800

    Update urls.py

--- autocomplete/urls.py
+++ autocomplete/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path("document/", views.render_doc_editor, name="render-doc-editor"),
     path("generate/", views.generate_text, name="generate-text"),
 ]
+
